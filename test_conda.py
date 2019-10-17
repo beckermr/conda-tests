f1c87aae0299a2d9851963095577f5e29584b2e7
Matthew R Becker <becker.mr@gmail.com>
2019-10-17 07:09:08 -0500

    more tests

---
+++ test_conda.py
@@ -0,0 +1,15 @@
+import os
+import pytest
+
+CONDA_BLD = os.path.join(os.environ['HOME'], 'miniconda', 'conda-bld')
+
+
+@pytest.mark.parametrize('pyver', ['27', '36', '37'])
+def test_a_python_versions(pyver):
+    pname = os.path.join(CONDA_BLD, 'linux-64', 'a-1-py%s_0.tar.bz2' % pyver)
+    assert os.path.exists(pname)
+
+
+def test_b_exists(pyver):
+    pname = os.path.join(CONDA_BLD, 'linux-64', 'b-1-py37_0.tar.bz2')
+    assert os.path.exists(pname)
