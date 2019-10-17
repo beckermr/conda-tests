c25610dd22c47e359905fb388dd97d2640f4dac7
Matthew R Becker <becker.mr@gmail.com>
2019-10-17 08:25:41 -0500

    all in pytest

---
+++ test_complicated_builds.py
@@ -0,0 +1,20 @@
+import subprocess
+import os
+
+CONDA_BLD = os.path.join(os.environ['HOME'], 'miniconda', 'conda-bld')
+TESTS = os.path.join(os.environ['HOME'], 'tests')
+
+
+def test_build_c():
+    subprocess.run("rm -f ${HOME}/tests/*", shell=True, check=True)
+    subprocess.run(
+        "conda build --use-local recipes/pkg_c", shell=True, check=True)
+
+    # this one runs downstreams
+    assert os.path.exists(TESTS, 'a_tests_ran')
+    assert os.path.exists(TESTS, 'b_tests_ran')
+    assert os.path.exists(TESTS, 'c_tests_ran')
+
+    pname = os.path.join(
+        CONDA_BLD, 'linux-64', 'c-1-py37_0.tar.bz2')
+    assert os.path.exists(pname)
---
+++ test_simple_builds.py
@@ -0,0 +1,28 @@
+import subprocess
+import os
+
+CONDA_BLD = os.path.join(os.environ['HOME'], 'miniconda', 'conda-bld')
+TESTS = os.path.join(os.environ['HOME'], 'tests')
+
+
+def test_build_a():
+    subprocess.run("rm -f ${HOME}/tests/*", shell=True, check=True)
+    subprocess.run("conda build recipes/pkg_a", shell=True, check=True)
+
+    assert os.path.exists(TESTS, 'a_tests_ran')
+
+    for pyver in ['27', '36', '37']:
+        pname = os.path.join(
+            CONDA_BLD, 'linux-64', 'a-1-py%s_0.tar.bz2' % pyver)
+        assert os.path.exists(pname)
+
+
+def test_build_b():
+    subprocess.run("rm -f ${HOME}/tests/*", shell=True, check=True)
+    subprocess.run("conda build recipes/pkg_b", shell=True, check=True)
+
+    assert os.path.exists(TESTS, 'b_tests_ran')
+
+    pname = os.path.join(
+        CONDA_BLD, 'linux-64', 'b-1-py37_0.tar.bz2')
+    assert os.path.exists(pname)
