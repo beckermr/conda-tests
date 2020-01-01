728582245b530fef0f778eba05b181048e74dc57
Matthew R Becker <becker.mr@gmail.com>
2020-01-01 16:39:27 -0600

    ENH add test of stubby symlinks

--- test_simple_builds.py
+++ test_simple_builds.py
@@ -48,3 +48,14 @@ def test_build_y():
     pname = os.path.join(
         CONDA_BLD, 'noarch', 'y-1-py_0.tar.bz2')
     assert os.path.exists(pname)
+
+
+def test_build_f():
+    subprocess.run("rm -f ${HOME}/tests/*", shell=True, check=True)
+    subprocess.run("conda build recipes/pkg_f", shell=True, check=True)
+
+    assert os.path.exists(os.path.join(TESTS, 'f_tests_ran'))
+
+    pname = os.path.join(
+        CONDA_BLD, 'linux-64', 'f-1-py37_0.tar.bz2')
+    assert os.path.exists(pname)
